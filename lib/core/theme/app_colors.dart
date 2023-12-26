9684decd87dbb7311e77b0244bdf99dda6c5db8d
AhmetSBulbul <ahmetsafabulbul@gmail.com>
2023-12-26 13:30:03 +0300

    init with fonts and colors

---
+++ lib/core/theme/app_colors.dart
@@ -0,0 +1,11 @@
+import 'dart:ui';
+
+abstract final class AppColors {
+  static const Color primary = Color(0xFFF63D68);
+  static const Color secondary = Color(0xFF246BFD);
+  static const Color black = Color(0xFF161616);
+  static const Color white = Color(0xFFFFFFFF);
+  static const Color grey = Color(0xFF5D5C64);
+  static const Color lightGrey = Color(0xFFB6B6B6);
+  static const Color surface = Color(0xFF1E1E1E);
+}
